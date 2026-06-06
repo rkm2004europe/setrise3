cba01ebc04008b56ae5fb160cf26334048b8064c
rkm2004europe <zexhup7@gmail.com>
2026-06-06 21:58:46 +0100

    Create app_colors.dart

---
+++ lib/features/shar/theme/app_colors.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class SharColors {
+  SharColors._();
+
+  static const Color background = Color(0xFF0D0D0D);
+  static const Color surface = Color(0xFF1C1C1E);
+  static const Color surfaceLight = Color(0xFF2C2C2E);
+  static const Color textPrimary = Colors.white;
+  static const Color textSecondary = Color(0xFF8E8E93);
+  static const Color textHint = Color(0xFF636366);
+  static const Color border = Color(0xFF38383A);
+  static const Color divider = Color(0xFF3A3A3C);
+  static const Color success = Color(0xFF34C759);
+  static const Color red = Color(0xFFFF453A);
+}
