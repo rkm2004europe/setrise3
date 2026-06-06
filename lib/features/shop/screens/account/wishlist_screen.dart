c483ac468d7f5b815ac26c5c0a1f3b4dc6c3d3eb
Adama1799 <139452063+Adama1799@users.noreply.github.com>
2026-06-06 02:49:16 +0100

    Fix comment formatting in wishlist_screen.dart

--- lib/features/shop/screens/account/wishlist_screen.dart
+++ lib/features/shop/screens/account/wishlist_screen.dart
@@ -1,4 +1,4 @@
-Enter// lib/features/shop/screens/account/wishlist_screen.dart
+// lib/features/shop/screens/account/wishlist_screen.dart
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
