362218e4a4560873deebe96fbb8bdf8db36b8e9d
Adama1799 <139452063+Adama1799@users.noreply.github.com>
2026-06-06 03:19:29 +0100

    Create ShopScreen widget
    Implement basic ShopScreen that wraps ShopHomeScreen.

---
+++ lib/features/shop/screens/shop_screen.dart
@@ -0,0 +1,13 @@
+// lib/features/shop/screens/shop_screen.dart
+// wrapper بسيط مثل الأصل
+
+import 'package:flutter/cupertino.dart';
+import 'package:setrise/features/shop/screens/shop_home_screen.dart';
+
+class ShopScreen extends StatelessWidget {
+  const ShopScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) => const ShopHomeScreen();
+}
+
