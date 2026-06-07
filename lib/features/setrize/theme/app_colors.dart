3e57c290c52cc6ef39d06b6874a14b840df0fa2e
rkm2004europe <zexhup7@gmail.com>
2026-06-07 01:02:55 +0100

    Add app_colors.dart to theme directory

---
+++ lib/features/setrize/theme/app_colors.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class SetColors {
+  SetColors._();
+  static const Color background = Color(0xFF0D0D0D);
+  static const Color surface = Color(0xFF1C1C1E);
+  static const Color textPrimary = Colors.white;
+  static const Color textSecondary = Color(0xFF8E8E93);
+  static const Color border = Color(0xFF38383A);
+  static const Color accent = Color(0xFF6C63FF);
+  static const Color neonGreen = Color(0xFF34C759);
+  static const Color white = Colors.white;
+  static const Color black = Colors.black;
+  static const Color grey = Color(0xFF2C2C2E);
+  static const Color grey2 = Color(0xFF8E8E93);
+}
