4b544aa7e198466561100a426700211bf274fb09
rkm2004europe <zexhup7@gmail.com>
2026-06-07 01:56:39 +0100

    Add PostColors class for theme colors

---
+++ lib/features/post/theme/app_colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class PostColors {
+  PostColors._();
+  static const Color background = Color(0xFF0D0D0D);
+  static const Color surface = Color(0xFF1C1C1E);
+  static const Color textPrimary = Colors.white;
+  static const Color textSecondary = Color(0xFF8E8E93);
+  static const Color accent = Color(0xFF6C63FF);
+  static const Color red = Color(0xFFFF453A);
+}
