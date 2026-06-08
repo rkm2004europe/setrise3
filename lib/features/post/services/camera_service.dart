9091227ac27ba83cc1d55c65726710861a96aedc
rkm2004europe <zexhup7@gmail.com>
2026-06-08 01:54:52 +0100

    Add CameraService with photo and video capture methods

---
+++ lib/features/post/services/camera_service.dart
@@ -0,0 +1,11 @@
+class CameraService {
+  Future<String?> capturePhoto() async {
+    // TODO: implement camera capture
+    return null;
+  }
+
+  Future<String?> captureVideo() async {
+    // TODO: implement video capture
+    return null;
+  }
+}
