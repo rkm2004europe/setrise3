1e3b00b88634a69a3c303d41ca13e22a7c1eeedd
rkm2004europe <zexhup7@gmail.com>
2026-06-08 16:39:35 +0100

    Create RizeListModel class for news feature

---
+++ lib/features/news/models/rize_list_model.dart
@@ -0,0 +1,13 @@
+class RizeListModel {
+  final String id;
+  final String name;
+  final String description;
+  final int postsCount;
+
+  const RizeListModel({
+    required this.id,
+    required this.name,
+    required this.description,
+    required this.postsCount,
+  });
+}
