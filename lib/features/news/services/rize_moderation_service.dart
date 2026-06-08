bbbf1ecfb5376f20eac1e94c8594c7e565cc302a
rkm2004europe <zexhup7@gmail.com>
2026-06-08 23:31:56 +0100

    Create RizeModerationService for post management

---
+++ lib/features/news/services/rize_moderation_service.dart
@@ -0,0 +1,9 @@
+class RizeModerationService {
+  Future<void> reportPost(String postId) async {
+    await Future.delayed(const Duration(seconds: 1));
+  }
+
+  Future<void> deletePost(String postId) async {
+    await Future.delayed(const Duration(seconds: 1));
+  }
+}
