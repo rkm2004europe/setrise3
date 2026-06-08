656eaa5243074d2ae827e6a6e880024618a0c754
rkm2004europe <zexhup7@gmail.com>
2026-06-08 02:00:35 +0100

    Add HashtagService with trending and suggestion methods

---
+++ lib/features/post/services/hashtag_service.dart
@@ -0,0 +1,10 @@
+class HashtagService {
+  List<String> getTrendingHashtags() {
+    return ['trending', 'viral', 'setrise', 'challenge', 'newpost'];
+  }
+
+  List<String> generateSuggestions(String caption) {
+    // Simple AI placeholder
+    return ['setrise', 'creator', 'explore'];
+  }
+}
