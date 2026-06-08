e98a64182f9040a605339c1427c76603ea6310fa
rkm2004europe <zexhup7@gmail.com>
2026-06-08 16:42:38 +0100

    Add mock data for RizeListModel

---
+++ lib/features/news/data/mock_rize_lists.dart
@@ -0,0 +1,22 @@
+import '../models/rize_list_model.dart';
+
+const List<RizeListModel> mockLists = [
+  RizeListModel(
+    id: 'l1',
+    name: 'Tech News',
+    description: 'أخبار التكنولوجيا اليومية',
+    postsCount: 234,
+  ),
+  RizeListModel(
+    id: 'l2',
+    name: 'Inspiration',
+    description: 'اقتباسات وتحفيز',
+    postsCount: 567,
+  ),
+  RizeListModel(
+    id: 'l3',
+    name: 'Flutter Tips',
+    description: 'نصائح يومية في فلاتر',
+    postsCount: 89,
+  ),
+];
