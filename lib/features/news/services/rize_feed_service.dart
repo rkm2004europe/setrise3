3efbb28d7575567501f1de3fd1b12871c0ade9f4
rkm2004europe <zexhup7@gmail.com>
2026-06-08 16:57:47 +0100

    Add fetchTrending and fetchFollowing methods

--- lib/features/news/services/rize_feed_service.dart
+++ lib/features/news/services/rize_feed_service.dart
@@ -3,8 +3,17 @@ import '../data/mock_rize_posts.dart';
 
 class RizeFeedService {
   Future<List<RizePostModel>> fetchFeed() async {
-    // محاكاة استدعاء API
     await Future.delayed(const Duration(seconds: 1));
     return generateMockRizePosts();
+  }
+
+  Future<List<RizePostModel>> fetchTrending() async {
+    await Future.delayed(const Duration(milliseconds: 500));
+    return generateMockRizePosts();
+  }
+
+  Future<List<RizePostModel>> fetchFollowing() async {
+    await Future.delayed(const Duration(milliseconds: 800));
+    return generateMockRizePosts().sublist(0, 5);
   }
 }
