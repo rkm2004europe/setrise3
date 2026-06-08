c0fbcf1683e25346c72ed83918b5ff0d42763aad
rkm2004europe <zexhup7@gmail.com>
2026-06-08 01:53:59 +0100

    Create MediaService with image and video picker methods

---
+++ lib/features/post/services/media_service.dart
@@ -0,0 +1,11 @@
+class MediaService {
+  Future<String?> pickImage() async {
+    // TODO: implement image picker
+    return null;
+  }
+
+  Future<String?> pickVideo() async {
+    // TODO: implement video picker
+    return null;
+  }
+}
