d09970ca73462aabbbb0dd50cc6adb94501d3d72
rkm2004europe <zexhup7@gmail.com>
2026-06-08 05:32:01 +0100

    Add trending_hashtags.dart file

---
+++ lib/features/news/data/trending_hashtags.dart
@@ -0,0 +1,12 @@
+const List<String> trendingHashtags = [
+  '#Flutter',
+  '#SetRise',
+  '#Tech',
+  '#Threads',
+  '#UIUX',
+  '#CodeNewbie',
+  '#AI',
+  '#Productivity',
+  '#Startup',
+  '#IndieDev',
+];
