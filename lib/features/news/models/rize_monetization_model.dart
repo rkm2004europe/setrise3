e3157a096d2ec5c95aa688d629c51dcebe872b42
rkm2004europe <zexhup7@gmail.com>
2026-06-09 02:35:19 +0100

    Add RizeMonetizationModel class for monetization data

---
+++ lib/features/news/models/rize_monetization_model.dart
@@ -0,0 +1,11 @@
+class RizeMonetizationModel {
+  final double totalEarnings;
+  final int impressions;
+  final double cpm;
+
+  const RizeMonetizationModel({
+    required this.totalEarnings,
+    required this.impressions,
+    required this.cpm,
+  });
+}
