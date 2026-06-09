2d84bc4d3a6e83162a7a22d619cd11b8cf0596ab
rkm2004europe <zexhup7@gmail.com>
2026-06-09 02:28:40 +0100

    Create RizeExploreModel class for news feature

---
+++ lib/features/news/models/rize_explore_model.dart
@@ -0,0 +1,15 @@
+class RizeExploreModel {
+  final String id;
+  final String title;
+  final String description;
+  final String imageUrl;
+  final String category;
+
+  const RizeExploreModel({
+    required this.id,
+    required this.title,
+    required this.description,
+    required this.imageUrl,
+    required this.category,
+  });
+}
