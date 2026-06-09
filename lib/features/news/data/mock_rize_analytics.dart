34a15767133c47125ba2c9eae091ae4d29f3b6a2
rkm2004europe <zexhup7@gmail.com>
2026-06-09 02:25:45 +0100

    Add mock data for RizeAnalyticsModel

---
+++ lib/features/news/data/mock_rize_analytics.dart
@@ -0,0 +1,10 @@
+import '../models/rize_analytics_model.dart';
+
+const mockAnalytics = RizeAnalyticsModel(
+  totalLikes: 12500,
+  totalComments: 3400,
+  totalShares: 890,
+  profileViews: 45000,
+  newFollowers: 230,
+  dailyLikes: [45, 67, 89, 120, 56, 78, 92],
+);
