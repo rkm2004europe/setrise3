214394c11f48a42c718361b7df62239f931e9e8a
rkm2004europe <zexhup7@gmail.com>
2026-06-09 02:20:22 +0100

    Create mock_rize_spaces.dart with sample data
    Added mock data for RizeSpaceModel to facilitate testing.

---
+++ lib/features/news/data/mock_rize_spaces.dart
@@ -0,0 +1,7 @@
+import '../models/rize_space_model.dart';
+
+const List<RizeSpaceModel> mockSpaces = [
+  RizeSpaceModel(id: 's1', title: 'مستقبل Flutter', hostUserId: 'u1', hostUserName: 'Ahmed K.', participants: ['u2', 'u3'], listenersCount: 230, isLive: true),
+  RizeSpaceModel(id: 's2', title: 'الذكاء الاصطناعي في 2026', hostUserId: 'u4', hostUserName: 'Lina R.', participants: ['u5'], listenersCount: 120, isLive: true),
+  RizeSpaceModel(id: 's3', title: 'ريادة الأعمال', hostUserId: 'u6', hostUserName: 'Omar T.', participants: [], listenersCount: 0, isLive: false),
+];
