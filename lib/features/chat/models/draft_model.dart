1a64677935a46140fadf83f680f89bcfa669ee16
rkm2004europe <zexhup7@gmail.com>
2026-06-13 01:41:21 +0100

    Create DraftModel class for chat feature

---
+++ lib/features/chat/models/draft_model.dart
@@ -0,0 +1,7 @@
+class DraftModel {
+  final String conversationId;
+  final String text;
+  final DateTime savedAt;
+
+  DraftModel({required this.conversationId, required this.text, required this.savedAt});
+}
