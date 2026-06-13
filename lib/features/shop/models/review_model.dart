5b4512bd229280ad3c8cc4f6b718b3ab7dcd8e23
rkm2004europe <zexhup7@gmail.com>
2026-06-13 23:30:59 +0100

    Create review_model.dart

---
+++ lib/features/shop/models/review_model.dart
@@ -0,0 +1,19 @@
+class ReviewModel {
+  final String id;
+  final String productId;
+  final String userId;
+  final String userName;
+  final double rating;
+  final String comment;
+  final DateTime createdAt;
+
+  ReviewModel({
+    required this.id,
+    required this.productId,
+    required this.userId,
+    required this.userName,
+    required this.rating,
+    required this.comment,
+    required this.createdAt,
+  });
+}
