6e6b91ef59ed026ec20ef6be9480f49eb708350d
rkm2004europe <zexhup7@gmail.com>
2026-06-13 23:49:13 +0100

    Implement ShopApiService to fetch products

---
+++ lib/features/shop/services/shop_api_service.dart
@@ -0,0 +1,9 @@
+import '../models/product_model.dart';
+import '../data/mock_products.dart';
+
+class ShopApiService {
+  Future<List<ProductModel>> fetchProducts() async {
+    await Future.delayed(const Duration(seconds: 1));
+    return mockProducts;
+  }
+}
