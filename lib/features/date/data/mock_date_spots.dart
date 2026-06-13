a1e73cdbe4435c8191f033264c292f06ed47bbc7
rkm2004europe <zexhup7@gmail.com>
2026-06-13 21:36:59 +0100

    Add mock date spots data for testing

---
+++ lib/features/date/data/mock_date_spots.dart
@@ -0,0 +1,7 @@
+import '../models/date_spot_model.dart';
+
+final List<DateSpotModel> mockDateSpots = [
+  DateSpotModel(id: 'sp1', name: 'مقهى النجوم', type: 'مقهى', address: 'الجزائر العاصمة', rating: 4.5, imageEmoji: '☕', checkins: 230, distance: 2.5),
+  DateSpotModel(id: 'sp2', name: 'حديقة التسلية', type: 'حديقة', address: 'الجزائر العاصمة', rating: 4.2, imageEmoji: '🌳', checkins: 560, distance: 5.0),
+  DateSpotModel(id: 'sp3', name: 'مطعم الباستا', type: 'مطعم', address: 'الجزائر العاصمة', rating: 4.8, imageEmoji: '🍝', checkins: 120, distance: 1.8),
+];
