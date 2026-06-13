93cf8923c0ada757eb76df72b644f123e10cdf4d
rkm2004europe <zexhup7@gmail.com>
2026-06-13 02:28:18 +0100

    Create SetRiseApp with dark theme and MainScreen
    Added SetRiseApp class with MaterialApp configuration.

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,1 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import '../../features/home/screens/main_screen.dart';
 
+class SetRiseApp extends StatelessWidget {
+  const SetRiseApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(
+      const SystemUiOverlayStyle(
+        statusBarColor: Colors.transparent,
+        statusBarIconBrightness: Brightness.light,
+        systemNavigationBarColor: Colors.black,
+        systemNavigationBarIconBrightness: Brightness.light,
+      ),
+    );
+
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        scaffoldBackgroundColor: const Color(0xFF0D0D0D),
+        fontFamily: 'Inter',
+      ),
+      home: const MainScreen(),
+    );
+  }
+}
