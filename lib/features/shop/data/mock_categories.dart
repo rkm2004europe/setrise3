b91ac505746fd3275605c48bbf2ce814946c59e1
rkm2004europe <zexhup7@gmail.com>
2026-06-13 23:10:19 +0100

    Add mock categories data for shop feature

---
+++ lib/features/shop/data/mock_categories.dart
@@ -0,0 +1,11 @@
+const List<Map<String, String>> categories = [
+  {'name': 'إلكترونيات', 'icon': '💻'},
+  {'name': 'أزياء', 'icon': '👗'},
+  {'name': 'طعام', 'icon': '🍔'},
+  {'name': 'سيارات', 'icon': '🚗'},
+  {'name': 'أثاث', 'icon': '🛋️'},
+  {'name': 'رياضة', 'icon': '⚽'},
+  {'name': 'توصيل', 'icon': '🛵'},
+  {'name': 'مزادات', 'icon': '🔨'},
+  {'name': 'سوق مفتوح', 'icon': '🏪'},
+];
