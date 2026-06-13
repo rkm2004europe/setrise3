123d0c3ebd5810a28bf60542d39f6e7ec7fd5ca9
rkm2004europe <zexhup7@gmail.com>
2026-06-13 01:42:12 +0100

    Add mock blocked users data

---
+++ lib/features/chat/data/mock_blocked.dart
@@ -0,0 +1,6 @@
+import '../models/models.dart';
+
+final List<User> mockBlockedUsers = [
+  User(id: 'b1', name: 'Spam Account', username: '@spam', avatar: '🚫'),
+  User(id: 'b2', name: 'Unknown', username: '@unknown', avatar: '❓'),
+];
