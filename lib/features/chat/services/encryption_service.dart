a67c5c09e1440320b1bfc94a27e3ed64c53907e7
rkm2004europe <zexhup7@gmail.com>
2026-06-13 01:38:09 +0100

    Create encryption_service.dart

---
+++ lib/features/chat/services/encryption_service.dart
@@ -0,0 +1,10 @@
+class EncryptionService {
+  String encrypt(String text) {
+    // TODO: implement real encryption
+    return text;
+  }
+
+  String decrypt(String text) {
+    return text;
+  }
+}
