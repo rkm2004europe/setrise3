3e714c6d9ea8a7d6b91e51b7023e5b7cc5327700
rkm2004europe <zexhup7@gmail.com>
2026-06-13 21:38:34 +0100

    Add mock questions for date feature

---
+++ lib/features/date/data/mock_questions.dart
@@ -0,0 +1,6 @@
+import '../models/date_question_model.dart';
+
+final List<DateQuestionModel> dailyQuestions = [
+  DateQuestionModel(id: 'q1', question: 'أفضل طريقة لقضاء المساء؟', options: ['فيلم', 'مطعم', 'مشي', 'موسيقى'], totalVotes: 340),
+  DateQuestionModel(id: 'q2', question: 'ما هو مشروبك المفضل؟', options: ['قهوة', 'شاي', 'عصير', 'ماء'], totalVotes: 520),
+];
