a761cd367668d354bc8aca485f762847862a4bf7
rkm2004europe <zexhup7@gmail.com>
2026-06-14 01:29:40 +0100

    Add mock data for followed stores

---
+++ lib/features/shop/data/mock_followed_stores.dart
@@ -0,0 +1,4 @@
+final List<Map<String, dynamic>> mockFollowedStores = [
+  {'name': 'متجر التقنية', 'products': 150, 'followers': 1200},
+  {'name': 'عالم الرياضة', 'products': 80, 'followers': 450},
+];
