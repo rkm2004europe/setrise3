3d7bd3c50d9b3b6578f6dc57ccb72583f7720f55
rkm2004europe <zexhup7@gmail.com>
2026-06-14 01:12:23 +0100

    Create order_service.dart

---
+++ lib/features/shop/widgets/order_service.dart
@@ -0,0 +1,14 @@
+import '../models/order_model.dart';
+import '../data/mock_orders.dart';
+
+class OrderService {
+  Future<List<OrderModel>> fetchOrders() async {
+    await Future.delayed(const Duration(seconds: 1));
+    return mockOrders;
+  }
+
+  Future<void> cancelOrder(String orderId) async {
+    await Future.delayed(const Duration(seconds: 1));
+    mockOrders.removeWhere((o) => o.id == orderId);
+  }
+}
