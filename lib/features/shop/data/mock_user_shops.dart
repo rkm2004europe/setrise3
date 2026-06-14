81cb7c11ba67240b4f1f37fb5f0de3d11c1a8666
rkm2004europe <zexhup7@gmail.com>
2026-06-15 00:57:37 +0100

    Add mock user shops data

---
+++ lib/features/shop/data/mock_user_shops.dart
@@ -0,0 +1,4 @@
+final List<Map<String, dynamic>> mockUserShops = [
+  {'userName': 'AhmedK', 'storeName': 'TechZone', 'productsCount': 25, 'followers': 340},
+  {'userName': 'SaraM', 'storeName': 'FashionHub', 'productsCount': 60, 'followers': 890},
+];
