a1356b699d778c6470c790549fca4c4227d1c1b0
rkm2004europe <zexhup7@gmail.com>
2026-06-15 00:51:54 +0100

    Add mock craftsmen data for shop feature

---
+++ lib/features/shop/data/mock_craftsmen.dart
@@ -0,0 +1,5 @@
+final List<Map<String, String>> mockCraftsmen = [
+  {'name': 'عمي محمد', 'icon': '🔨', 'craft': 'نجارة', 'rating': '4.8'},
+  {'name': 'الحاج لخضر', 'icon': '🪚', 'craft': 'حدادة', 'rating': '4.6'},
+  {'name': 'سلمى', 'icon': '🧵', 'craft': 'خياطة', 'rating': '4.9'},
+];
