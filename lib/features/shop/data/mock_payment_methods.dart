f8a8932ac4a92c946b1459047948b4c171728fab
rkm2004europe <zexhup7@gmail.com>
2026-06-14 01:28:48 +0100

    Add mock payment methods for shop feature

---
+++ lib/features/shop/data/mock_payment_methods.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import '../theme/app_colors.dart';
+
+final List<Map<String, dynamic>> mockPaymentMethods = [
+  {'id': 'cod', 'name': 'الدفع عند الاستلام', 'icon': Icons.handshake},
+  {'id': 'card', 'name': 'بطاقة ائتمان', 'icon': Icons.credit_card},
+  {'id': 'wallet', 'name': 'محفظة SetRise', 'icon': Icons.account_balance_wallet},
+];
