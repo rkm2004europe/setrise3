b8ba30a2b3d0dbdfcc47fa9474aab822628fe75d
rkm2004europe <zexhup7@gmail.com>
2026-06-15 00:57:04 +0100

    Add InstallmentPlanModel class for payment plans

---
+++ lib/features/shop/models/installment_plan_model.dart
@@ -0,0 +1,11 @@
+class InstallmentPlanModel {
+  final int months;
+  final double monthlyPayment;
+  final double total;
+
+  InstallmentPlanModel({
+    required this.months,
+    required this.monthlyPayment,
+    required this.total,
+  });
+}
