a6ca51aea2b5e3a5bcd903ea00825ca3569ba590
rkm2004europe <zexhup7@gmail.com>
2026-06-14 01:14:57 +0100

    Create PaymentMethodModel class for payment methods

---
+++ lib/features/shop/widgets/payment_method_model.dart
@@ -0,0 +1,7 @@
+class PaymentMethodModel {
+  final String id;
+  final String name;
+  final String icon; // emoji
+
+  PaymentMethodModel({required this.id, required this.name, required this.icon});
+}
