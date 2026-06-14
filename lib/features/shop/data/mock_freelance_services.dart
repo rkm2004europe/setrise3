c7c211e00c7bd44cf9171ca05f226feb2b1b42bc
rkm2004europe <zexhup7@gmail.com>
2026-06-15 00:50:37 +0100

    Add mock freelance services data

---
+++ lib/features/shop/data/mock_freelance_services.dart
@@ -0,0 +1,6 @@
+final List<Map<String, String>> mockFreelanceServices = [
+  {'name': 'تصميم شعار', 'icon': '🎨', 'category': 'تصميم', 'price': '50'},
+  {'name': 'برمجة تطبيق', 'icon': '💻', 'category': 'برمجة', 'price': '150'},
+  {'name': 'تسويق إلكتروني', 'icon': '📈', 'category': 'تسويق', 'price': '80'},
+  {'name': 'كتابة محتوى', 'icon': '✍️', 'category': 'كتابة', 'price': '30'},
+];
