df0688b663670bc8e3192aa060ec142bbe715f82
rkm2004europe <zexhup7@gmail.com>
2026-06-15 01:33:39 +0100

    Add ShoppingListModel class with name and products

---
+++ lib/features/shop/models/shopping_list_model.dart
@@ -0,0 +1,7 @@
+import 'product_model.dart';
+
+class ShoppingListModel {
+  final String name;
+  final List<ProductModel> products;
+  ShoppingListModel({required this.name, required this.products});
+}
