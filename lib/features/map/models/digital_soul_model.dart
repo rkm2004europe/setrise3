6a8ae79b767556bb452dbabdf0a2725428b3d3ea
rkm2004europe <zexhup7@gmail.com>
2026-06-15 17:20:16 +0100

    Create DigitalSoulModel class with user properties

---
+++ lib/features/map/models/digital_soul_model.dart
@@ -0,0 +1,15 @@
+class DigitalSoulModel {
+  final String userId;
+  final String name;
+  final double lat;
+  final double lng;
+  final String mood;
+
+  DigitalSoulModel({
+    required this.userId,
+    required this.name,
+    required this.lat,
+    required this.lng,
+    required this.mood,
+  });
+}
