293221e6c774bdd521443ad4b14341854e8d6ba8
rkm2004europe <zexhup7@gmail.com>
2026-06-15 17:21:02 +0100

    Add mock data for SerendipityModel

---
+++ lib/features/map/data/mock_serendipity.dart
@@ -0,0 +1,6 @@
+import '../models/serendipity_model.dart';
+
+final List<SerendipityModel> mockSerendipity = [
+  SerendipityModel(userId: 's1', userName: 'Lina', avatar: '👩‍🦰', compatibilityScore: 87, sharedInterests: ['موسيقى', 'سفر']),
+  SerendipityModel(userId: 's2', userName: 'Karim', avatar: '🧔', compatibilityScore: 65, sharedInterests: ['رياضة', 'ألعاب']),
+];
