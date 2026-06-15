2aacc81856e765c940f262f4088ce8488e6d765a
rkm2004europe <zexhup7@gmail.com>
2026-06-15 01:34:15 +0100

    Add mock Q&A data for shop feature

---
+++ lib/features/shop/data/mock_qna.dart
@@ -0,0 +1,5 @@
+import '../models/qna_model.dart';
+final List<QnaModel> mockQna = [
+  QnaModel(question: 'هل يدعم الشحن الدولي؟', answer: 'نعم، الشحن متاح لكل الدول.'),
+  QnaModel(question: 'ما هي مدة الضمان؟', answer: 'سنة كاملة ضد عيوب التصنيع.'),
+];
