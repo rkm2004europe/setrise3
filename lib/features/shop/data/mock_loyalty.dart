027416db84e809664f835486b960ac71f8368db6
rkm2004europe <zexhup7@gmail.com>
2026-06-15 01:35:18 +0100

    Add mock loyalty data for testing purposes

---
+++ lib/features/shop/data/mock_loyalty.dart
@@ -0,0 +1,5 @@
+import '../models/loyalty_model.dart';
+final LoyaltyModel mockLoyalty = LoyaltyModel(points: 250, rewards: [
+  {'discount': 5, 'points': 100},
+  {'discount': 15, 'points': 250},
+]);
