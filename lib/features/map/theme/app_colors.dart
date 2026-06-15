f00af61817ad923668b85f942d58db74fb8d579d
rkm2004europe <zexhup7@gmail.com>
2026-06-15 17:04:56 +0100

    Create app_colors.dart

---
+++ lib/features/map/theme/app_colors.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class MapColors {
+  MapColors._();
+  static const Color bg = Color(0xFF0D0D0D);
+  static const Color surface = Color(0xFF1C1C1E);
+  static const Color text = Colors.white;
+  static const Color text2 = Color(0xFF8E8E93);
+  static const Color accent = Color(0xFF6C63FF);
+  static const Color red = Color(0xFFFF3B30);
+  static const Color green = Color(0xFF34C759);
+  static const Color orange = Color(0xFFFF9500);
+  static const Color gold = Color(0xFFFFD60A);
+  static const Color blue = Color(0xFF007AFF);
+  static const Color border = Color(0xFF38383A);
+}
