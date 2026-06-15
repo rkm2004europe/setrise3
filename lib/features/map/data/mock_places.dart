22a6ef52f3a868caea4ffb528df2bb53bc51a22c
rkm2004europe <zexhup7@gmail.com>
2026-06-15 17:11:59 +0100

    Add mock places data for testing

---
+++ lib/features/map/data/mock_places.dart
@@ -0,0 +1,6 @@
+import '../models/place_model.dart';
+
+final List<PlaceModel> mockPlaces = [
+  PlaceModel(id: 'pl1', name: 'مطعم الباستا', type: 'مطعم', lat: 36.7510, lng: 3.0530, address: 'شارع ديدوش مراد', rating: 4.5, reviewCount: 120, imageEmoji: '🍝', hasPromo: true, promoText: 'خصم 20%'),
+  PlaceModel(id: 'pl2', name: 'متجر التقنية', type: 'متجر', lat: 36.7550, lng: 3.0580, address: 'ساحة الشهداء', rating: 4.2, reviewCount: 89, imageEmoji: '💻'),
+];
