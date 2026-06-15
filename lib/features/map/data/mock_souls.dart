032922a7be4275347a146f2ef018822557b4eb0f
rkm2004europe <zexhup7@gmail.com>
2026-06-15 17:22:35 +0100

    Add mock souls data for DigitalSoulModel

---
+++ lib/features/map/data/mock_souls.dart
@@ -0,0 +1,6 @@
+import '../models/digital_soul_model.dart';
+
+final List<DigitalSoulModel> mockSouls = [
+  DigitalSoulModel(userId: 'ds1', name: 'روح أحمد', lat: 36.754, lng: 3.059, mood: 'سعيد'),
+  DigitalSoulModel(userId: 'ds2', name: 'روح سارة', lat: 36.750, lng: 3.055, mood: 'متحمس'),
+];
