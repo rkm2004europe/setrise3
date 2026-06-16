887a701ec18133ceefd753fbc53129ef03a5d2f7
rkm2004europe <zexhup7@gmail.com>
2026-06-16 02:05:36 +0100

    Add mock data for AR filters

---
+++ lib/features/live/data/mock_ar_filters.dart
@@ -0,0 +1,7 @@
+import '../models/ar_filter_model.dart';
+
+final List<ArFilterModel> mockArFilters = [
+  ArFilterModel(id: 'ar1', name: 'نظارات', thumbnailEmoji: '👓'),
+  ArFilterModel(id: 'ar2', name: 'قبعة', thumbnailEmoji: '🎩', isPremium: true),
+  ArFilterModel(id: 'ar3', name: 'تاج', thumbnailEmoji: '👑', isPremium: true),
+];
