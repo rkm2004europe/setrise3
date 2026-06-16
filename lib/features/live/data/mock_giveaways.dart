674a08d7e2ddf5e4ba2b45f290f1f740bfef7d05
rkm2004europe <zexhup7@gmail.com>
2026-06-16 02:05:09 +0100

    Add mock giveaways data for testing

---
+++ lib/features/live/data/mock_giveaways.dart
@@ -0,0 +1,5 @@
+import '../models/giveaway_model.dart';
+
+final List<GiveawayModel> mockGiveaways = [
+  GiveawayModel(id: 'gw1', hostId: 'h1', prize: '50 🪙', winnersCount: 3, participants: ['v1', 'v2', 'v3', 'v4']),
+];
