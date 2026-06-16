b0a5583e31c6a9d7e470bcf3d98eff4e10e4afa5
rkm2004europe <zexhup7@gmail.com>
2026-06-16 02:04:41 +0100

    Add mock battles data for LiveBattleModel

---
+++ lib/features/live/data/mock_battles.dart
@@ -0,0 +1,6 @@
+import '../models/battle_model.dart';
+
+final List<LiveBattleModel> mockBattles = [
+  LiveBattleModel(id: 'b1', host1Id: 'h1', host2Id: 'h2', host1Score: 120, host2Score: 95),
+  LiveBattleModel(id: 'b2', host1Id: 'h3', host2Id: 'h4', host1Score: 80, host2Score: 110),
+];
