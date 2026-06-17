43c22fe5a5949bd4692b35058543fac5374f7c84
rkm2004europe <zexhup7@gmail.com>
2026-06-17 01:12:46 +0100

    Add LeaderboardEntry model class

---
+++ lib/features/live/models/leaderboard_model.dart
@@ -0,0 +1,7 @@
+class LeaderboardEntry {
+  final String userId;
+  final String userName;
+  final String avatar;
+  final int coins;
+  LeaderboardEntry({required this.userId, required this.userName, required this.avatar, required this.coins});
+}
