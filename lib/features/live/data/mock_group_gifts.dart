e1eecc304d544dbe955069577f4b3737ff1ac555
rkm2004europe <zexhup7@gmail.com>
2026-06-18 15:14:03 +0100

    Add mock group gifts data

---
+++ lib/features/live/data/mock_group_gifts.dart
@@ -0,0 +1,5 @@
+import '../models/group_gift_model.dart';
+
+final List<GroupGiftModel> mockGroupGifts = [
+  GroupGiftModel(id: 'gg1', giftName: 'صاروخ', totalCost: 5000, currentContributors: 6, requiredContributors: 10),
+];
