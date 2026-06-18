9880ff217903dfab328cca9b4b7532857095e688
rkm2004europe <zexhup7@gmail.com>
2026-06-18 14:53:30 +0100

    Add mock data for viewer challenges

---
+++ lib/features/live/data/mock_viewer_challenges.dart
@@ -0,0 +1,6 @@
+import '../models/viewer_challenge_model.dart';
+
+final List<ViewerChallengeModel> mockViewerChallenges = [
+  ViewerChallengeModel(id: 'vc1', hostId: 'h1', description: 'غني أغنية!', rewardCoins: 50, acceptedBy: ['v1', 'v2']),
+  ViewerChallengeModel(id: 'vc2', hostId: 'h1', description: 'أكمل 20 تمرين ضغط', rewardCoins: 100),
+];
