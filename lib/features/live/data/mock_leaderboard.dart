ca3488f1149b48c347d2c0ff2085ece88888288d
rkm2004europe <zexhup7@gmail.com>
2026-06-18 15:23:58 +0100

    Add mock leaderboard data for testing

---
+++ lib/features/live/data/mock_leaderboard.dart
@@ -0,0 +1,6 @@
+import '../models/leaderboard_model.dart';
+
+final List<LeaderboardEntry> mockLeaderboard = [
+  LeaderboardEntry(userId: 'u1', userName: 'Ahmed', avatar: '🧑', coins: 3400),
+  LeaderboardEntry(userId: 'u2', userName: 'Sara', avatar: '👩', coins: 2800),
+];
