c6014c27c65629cf3bcfa36fab8c3f645bd25ffa
rkm2004europe <zexhup7@gmail.com>
2026-06-18 15:22:21 +0100

    Add mock subscriptions data for testing

---
+++ lib/features/live/data/mock_subscriptions.dart
@@ -0,0 +1,5 @@
+import '../models/subscription_model.dart';
+
+final List<SubscriptionModel> mockSubscriptions = [
+  SubscriptionModel(id: 'sub1', hostId: 'h1', subscriberId: 'me', monthlyPrice: 4.99, startDate: DateTime.now()),
+];
