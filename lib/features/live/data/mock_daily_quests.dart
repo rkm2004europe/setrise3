89052ad1038a9e7bd57b05ae04f9e02e4943983c
rkm2004europe <zexhup7@gmail.com>
2026-06-18 14:59:44 +0100

    Add mock daily quests data

---
+++ lib/features/live/data/mock_daily_quests.dart
@@ -0,0 +1,6 @@
+import '../models/daily_quest_model.dart';
+
+final List<DailyQuestModel> mockDailyQuests = [
+  DailyQuestModel(id: 'dq1', description: 'شاهد 3 بثوث', xpReward: 50, target: 3, progress: 1),
+  DailyQuestModel(id: 'dq2', description: 'أرسل 5 هدايا', coinReward: 20, target: 5, progress: 3),
+];
