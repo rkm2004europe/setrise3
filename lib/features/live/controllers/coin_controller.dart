3cd94467f65ffeaba873e799828928ec515082dd
rkm2004europe <zexhup7@gmail.com>
2026-06-19 19:38:23 +0100

    Refactor spendCoins method to return boolean

--- lib/features/live/controllers/coin_controller.dart
+++ lib/features/live/controllers/coin_controller.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
+import '../data/mock_coins.dart';
 
 class CoinController extends ChangeNotifier {
   int _balance = mockUserCoins;
   int get balance => _balance;
 
   void addCoins(int amount) { _balance += amount; notifyListeners(); }
-  void spendCoins(int amount) { _balance -= amount; notifyListeners(); }
+  bool spendCoins(int amount) {
+    if (_balance >= amount) { _balance -= amount; notifyListeners(); return true; }
+    return false;
+  }
 }
