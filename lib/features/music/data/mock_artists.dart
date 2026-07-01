09610f156bcdf7993cfc3f5ed34398b27f07bab9
rkm2004europe <zexhup7@gmail.com>
2026-07-01 16:33:20 +0100

    Add mock artists data for testing

---
+++ lib/features/music/data/mock_artists.dart
@@ -0,0 +1,10 @@
+import '../models/artist_model.dart';
+
+final List<ArtistModel> mockArtists = [
+  ArtistModel(id: 'ar1', name: 'The Weeknd', avatarEmoji: '🧑‍🎤', monthlyListeners: 85000000, topTracks: ['Blinding Lights', 'Starboy'], isFollowed: true),
+  ArtistModel(id: 'ar2', name: 'Dua Lipa', avatarEmoji: '👩‍🎤', monthlyListeners: 65000000, topTracks: ['Levitating', 'Don\'t Start Now']),
+  ArtistModel(id: 'ar3', name: 'Eminem', avatarEmoji: '🧢', monthlyListeners: 72000000, topTracks: ['Lose Yourself', 'Stan'], isFollowed: true),
+  ArtistModel(id: 'ar4', name: 'Queen', avatarEmoji: '👨‍🎤', monthlyListeners: 55000000, topTracks: ['Bohemian Rhapsody']),
+  ArtistModel(id: 'ar5', name: 'M83', avatarEmoji: '🎹', monthlyListeners: 12000000, topTracks: ['Midnight City']),
+  ArtistModel(id: 'ar6', name: 'Imagine Dragons', avatarEmoji: '🐉', monthlyListeners: 48000000, topTracks: ['Radioactive']),
+];
