a1946c6927eacf47a9d264d1a676d8c0069477c5
rkm2004europe <zexhup7@gmail.com>
2026-07-01 16:46:17 +0100

    Add mock categories for music feature

---
+++ lib/features/music/data/mock_categories.dart
@@ -0,0 +1,12 @@
+import '../models/category_model.dart';
+
+final List<CategoryModel> mockCategories = [
+  CategoryModel(name: 'Pop', emoji: '🎤', colorHex: '0xFF007AFF'),
+  CategoryModel(name: 'Rock', emoji: '🎸', colorHex: '0xFFFF3B30'),
+  CategoryModel(name: 'Hip-Hop', emoji: '🎧', colorHex: '0xFFFF9500'),
+  CategoryModel(name: 'Electronic', emoji: '🎹', colorHex: '0xFF34C759'),
+  CategoryModel(name: 'R&B', emoji: '🎵', colorHex: '0xFFAF52DE'),
+  CategoryModel(name: 'Classic', emoji: '🎼', colorHex: '0xFF8E8E93'),
+  CategoryModel(name: 'Disco', emoji: '🕺', colorHex: '0xFFFF2D55'),
+  CategoryModel(name: 'Alternative', emoji: '🎭', colorHex: '0xFF00BCD4'),
+];
