27a4d80372275df22ed407342bd1f09e175153b7
rkm2004europe <zexhup7@gmail.com>
2026-07-01 16:34:16 +0100

    Add mock tracks data for music feature

---
+++ lib/features/music/data/mock_tracks.dart
@@ -0,0 +1,14 @@
+import '../models/track_model.dart';
+
+final List<TrackModel> mockTracks = [
+  TrackModel(id: 't1', title: 'Midnight City', artist: 'M83', album: 'Hurry Up, We\'re Dreaming', coverEmoji: '🌃', duration: const Duration(minutes: 4, seconds: 2), audioUrl: '', plays: 15000000, genres: ['Electronic', 'Synthwave']),
+  TrackModel(id: 't2', title: 'Blinding Lights', artist: 'The Weeknd', album: 'After Hours', coverEmoji: '🚗', duration: const Duration(minutes: 3, seconds: 22), audioUrl: '', plays: 32000000, genres: ['Pop', 'Synthwave']),
+  TrackModel(id: 't3', title: 'Bohemian Rhapsody', artist: 'Queen', album: 'A Night at the Opera', coverEmoji: '👑', duration: const Duration(minutes: 5, seconds: 55), audioUrl: '', plays: 45000000, genres: ['Rock', 'Classic']),
+  TrackModel(id: 't4', title: 'Shape of You', artist: 'Ed Sheeran', album: '÷', coverEmoji: '❤️', duration: const Duration(minutes: 3, seconds: 53), audioUrl: '', plays: 50000000, genres: ['Pop']),
+  TrackModel(id: 't5', title: 'Starboy', artist: 'The Weeknd', album: 'Starboy', coverEmoji: '⭐', duration: const Duration(minutes: 3, seconds: 50), audioUrl: '', plays: 28000000, genres: ['R&B', 'Pop']),
+  TrackModel(id: 't6', title: 'Toxic', artist: 'Britney Spears', album: 'In the Zone', coverEmoji: '🧪', duration: const Duration(minutes: 3, seconds: 19), audioUrl: '', plays: 22000000, genres: ['Pop']),
+  TrackModel(id: 't7', title: 'Hotel California', artist: 'Eagles', album: 'Hotel California', coverEmoji: '🏨', duration: const Duration(minutes: 6, seconds: 30), audioUrl: '', plays: 40000000, genres: ['Rock', 'Classic']),
+  TrackModel(id: 't8', title: 'Levitating', artist: 'Dua Lipa', album: 'Future Nostalgia', coverEmoji: '🪐', duration: const Duration(minutes: 3, seconds: 23), audioUrl: '', plays: 18000000, genres: ['Pop', 'Disco']),
+  TrackModel(id: 't9', title: 'Radioactive', artist: 'Imagine Dragons', album: 'Night Visions', coverEmoji: '☢️', duration: const Duration(minutes: 3, seconds: 6), audioUrl: '', plays: 35000000, genres: ['Rock', 'Alternative']),
+  TrackModel(id: 't10', title: 'Lose Yourself', artist: 'Eminem', album: '8 Mile', coverEmoji: '🎤', duration: const Duration(minutes: 5, seconds: 26), audioUrl: '', plays: 42000000, genres: ['Hip-Hop', 'Rap']),
+];
