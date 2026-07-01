ff65a697f424e998d259f41b98df013077ee575b
rkm2004europe <zexhup7@gmail.com>
2026-07-02 00:43:03 +0100

    Refactor LikeButton to use LibraryController for likes

--- lib/features/music/widgets/like_button.dart
+++ lib/features/music/widgets/like_button.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 import '../theme/app_colors.dart';
 import '../models/track_model.dart';
+import '../controllers/library_controller.dart';
 
 class LikeButton extends StatefulWidget {
   final TrackModel track;
-  const LikeButton({super.key, required this.track});
+  final LibraryController controller;
+  const LikeButton({super.key, required this.track, required this.controller});
 
   @override
   State<LikeButton> createState() => _LikeButtonState();
@@ -13,14 +15,11 @@ class LikeButton extends StatefulWidget {
 class _LikeButtonState extends State<LikeButton> {
   @override
   Widget build(BuildContext context) {
+    final isLiked = widget.controller.isLiked(widget.track.id);
     return GestureDetector(
-      onTap: () => setState(() => widget.track.isLiked = !widget.track.isLiked),
+      onTap: () => widget.controller.toggleLike(widget.track),
       child: Column(children: [
-        Icon(
-          widget.track.isLiked ? Icons.favorite : Icons.favorite_border,
-          color: widget.track.isLiked ? MusicColors.like : MusicColors.text2,
-          size: 24,
-        ),
+        Icon(isLiked ? Icons.favorite : Icons.favorite_border, color: isLiked ? MusicColors.like : MusicColors.text2, size: 24),
         const SizedBox(height: 4),
         const Text('Like', style: TextStyle(color: MusicColors.text2, fontSize: 11)),
       ]),
