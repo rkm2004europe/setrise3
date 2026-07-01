5ab869463aa2165a4091efdaea265ad3988f0230
rkm2004europe <zexhup7@gmail.com>
2026-07-01 16:47:28 +0100

    Create category_model.dart

---
+++ lib/features/music/models/category_model.dart
@@ -0,0 +1,7 @@
+class CategoryModel {
+  final String name;
+  final String emoji;
+  final String colorHex;
+
+  CategoryModel({required this.name, required this.emoji, required this.colorHex});
+}
