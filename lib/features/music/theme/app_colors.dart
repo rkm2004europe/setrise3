6f04209c9e03ba136ad2ba53240b473030869ae9
rkm2004europe <zexhup7@gmail.com>
2026-07-01 16:35:51 +0100

    Add MusicColors class for theme colors

---
+++ lib/features/music/theme/app_colors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class MusicColors {
+  MusicColors._();
+  static const Color bg = Color(0xFF0D0D0D);
+  static const Color surface = Color(0xFF1C1C1E);
+  static const Color text = Colors.white;
+  static const Color text2 = Color(0xFF8E8E93);
+  static const Color accent = Color(0xFF6C63FF);
+  static const Color like = Color(0xFFFF2D55);
+  static const Color border = Color(0xFF38383A);
+}
