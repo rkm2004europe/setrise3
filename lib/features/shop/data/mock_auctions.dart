f26d5630183c9950e5dfd9a4fdb9d7ffad6476d4
rkm2004europe <zexhup7@gmail.com>
2026-07-02 21:15:06 +0100

    Format mockAuctions for better readability

--- lib/features/shop/data/mock_auctions.dart
+++ lib/features/shop/data/mock_auctions.dart
@@ -2,9 +2,15 @@ import '../models/auction_model.dart';
 
 final List<AuctionModel> mockAuctions = [
   AuctionModel(
-    id: 'a1', productName: 'ساعة ذكية', image: '⌚', startingPrice: 200, currentBid: 350, highestBidderId: 'u1', endTime: DateTime.now().add(const Duration(hours: 2)), bidderIds: ['u1', 'u2', 'u3'],
+    id: 'a1', productName: 'ساعة ذكية', image: '⌚', startingPrice: 200,
+    currentBid: 350, highestBidderId: 'u1',
+    endTime: DateTime.now().add(const Duration(hours: 2)),
+    bidderIds: ['u1', 'u2', 'u3'],
   ),
   AuctionModel(
-    id: 'a2', productName: 'سماعات احترافية', image: '🎧', startingPrice: 100, currentBid: 180, highestBidderId: 'u5', endTime: DateTime.now().add(const Duration(minutes: 45)), bidderIds: ['u5', 'u6'],
+    id: 'a2', productName: 'سماعات احترافية', image: '🎧', startingPrice: 100,
+    currentBid: 180, highestBidderId: 'u5',
+    endTime: DateTime.now().add(const Duration(minutes: 45)),
+    bidderIds: ['u5', 'u6'],
   ),
 ];
