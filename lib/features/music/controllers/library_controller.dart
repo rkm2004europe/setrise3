47962114e5a864cf556eefdd2e38b72bd1686234
rkm2004europe <zexhup7@gmail.com>
2026-07-02 01:39:38 +0100

    Refactor LibraryController methods for simplicity
    Removed unused methods and simplified existing ones in LibraryController.

--- lib/features/music/controllers/library_controller.dart
+++ lib/features/music/controllers/library_controller.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import '../models/track_model.dart';
-import '../models/playlist_model.dart';
 import '../services/library_service.dart';
 
 class LibraryController extends ChangeNotifier {
@@ -8,27 +7,8 @@ class LibraryController extends ChangeNotifier {
 
   List<TrackModel> get likedSongs => _service.likedSongs;
   List<TrackModel> get recentlyPlayed => _service.recentlyPlayed;
-  List<PlaylistModel> get playlists => _service.playlists;
 
-  void toggleLike(TrackModel track) {
-    _service.toggleLike(track);
-    notifyListeners();
-  }
-
-  void removeLiked(String trackId) {
-    _service.removeLiked(trackId);
-    notifyListeners();
-  }
-
-  void addToRecentlyPlayed(TrackModel track) {
-    _service.addToRecentlyPlayed(track);
-    notifyListeners();
-  }
-
+  void toggleLike(TrackModel track) { _service.toggleLike(track); notifyListeners(); }
+  void addToRecentlyPlayed(TrackModel track) { _service.addToRecentlyPlayed(track); notifyListeners(); }
   bool isLiked(String id) => _service.isLiked(id);
-
-  void createPlaylist(String name) {
-    _service.createPlaylist(name);
-    notifyListeners();
-  }
 }
