30273f85c888695df09f6000926f79eaf114b639
rkm2004europe <zexhup7@gmail.com>
2026-07-02 01:30:43 +0100

    Add playlist management methods to LibraryController

--- lib/features/music/controllers/library_controller.dart
+++ lib/features/music/controllers/library_controller.dart
@@ -1,15 +1,34 @@
 import 'package:flutter/material.dart';
 import '../models/track_model.dart';
+import '../models/playlist_model.dart';
 import '../services/library_service.dart';
 
 class LibraryController extends ChangeNotifier {
   final LibraryService _service = LibraryService();
+
   List<TrackModel> get likedSongs => _service.likedSongs;
+  List<TrackModel> get recentlyPlayed => _service.recentlyPlayed;
+  List<PlaylistModel> get playlists => _service.playlists;
 
   void toggleLike(TrackModel track) {
     _service.toggleLike(track);
     notifyListeners();
   }
 
+  void removeLiked(String trackId) {
+    _service.removeLiked(trackId);
+    notifyListeners();
+  }
+
+  void addToRecentlyPlayed(TrackModel track) {
+    _service.addToRecentlyPlayed(track);
+    notifyListeners();
+  }
+
   bool isLiked(String id) => _service.isLiked(id);
+
+  void createPlaylist(String name) {
+    _service.createPlaylist(name);
+    notifyListeners();
+  }
 }
